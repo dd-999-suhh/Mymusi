3a2a9bc5eba9dfe54fb0037edcf51c9daf46ab0b
dd-999-suhh <julierbo791@gmail.com>
2026-09-08 08:18:16 +0630

    PANDA

--- KartikMusic/helpers/_thumbnails.py
+++ KartikMusic/helpers/_thumbnails.py
@@ -1,12 +1,3 @@
-#
-# Copyright (C) 2025-present by TheAloneTeam@Github, < https://github.com/TheAloneTeam >.
-#
-# This file is part of < https://github.com/TheAloneTeam/KartikMusic > project,
-# and is released under the "MIT License".
-# Please see < https://github.com/TheAloneTeam/KartikMusic/blob/master/LICENSE >
-#
-# All rights reserved.
-#
 
 import asyncio
 import os
@@ -20,14 +11,7 @@ from KartikMusic.helpers import Track
 
 class Thumbnail:
     def __init__(self):
-        self.rect = (914, 514)
-        self.fill = (255, 255, 255)
-        try:
-            self.font1 = ImageFont.truetype("KartikMusic/helpers/Raleway-Bold.ttf", 30)
-            self.font2 = ImageFont.truetype("KartikMusic/helpers/Inter-Light.ttf", 30)
-        except Exception:
-            self.font1 = ImageFont.load_default()
-            self.font2 = ImageFont.load_default()
+    
         self.session: aiohttp.ClientSession | None = None
 
     async def start(self) -> None:
@@ -43,67 +27,43 @@ class Thumbnail:
                 f.write(await resp.read())
         return output_path
 
-    def _draw_image(self, temp, output, song: Track, size=(1280, 720)):
-        thumb = (
-            Image.open(temp)
-            .convert("RGBA")
-            .resize(
-                size,
-                Image.Resampling.LANCZOS,
-            )
-        )
-        blur = thumb.filter(ImageFilter.GaussianBlur(25))
-        image = ImageEnhance.Brightness(blur).enhance(0.40)
+    
+    def _draw_image(self, temp, output, size=(1280, 720)):
+        
+        thumb = Image.open(temp).convert("RGB").resize(size, Image.Resampling.LANCZOS)
 
-        _rect = ImageOps.fit(
-            thumb,
-            self.rect,
-            method=Image.LANCZOS,
-            centering=(0.5, 0.5),
-        )
-        mask = Image.new("L", self.rect, 0)
-        ImageDraw.Draw(mask).rounded_rectangle(
-            (0, 0, self.rect[0], self.rect[1]),
-            radius=15,
-            fill=255,
-        )
-        _rect.putalpha(mask)
-        image.paste(_rect, (183, 30), _rect)
+        
 
-        draw = ImageDraw.Draw(image)
-        draw.text(
-            xy=(50, 560),
-            text=f"{(song.channel_name or 'Unknown')[:25]} | {song.view_count or 0}",
-            font=self.font2,
-            fill=self.fill,
-        )
-        draw.text(
-            (50, 600), (song.title or "Unknown")[:50], font=self.font1, fill=self.fill
-        )
-        draw.text((40, 650), "0:01", font=self.font1)
-        draw.line([(140, 670), (1160, 670)], fill=self.fill, width=5, joint="curve")
-        draw.text(
-            (1185, 650), song.duration or "00:00", font=self.font1, fill=self.fill
-        )
-
-        image.save(output)
+        
+        thumb.save(output)
         return output
 
     async def generate(self, song: Track, size=(1280, 720)) -> str:
         try:
-            temp = f"cache/temp_{song.id}.jpg"
+            
             output = f"cache/{song.id}.png"
+
             if os.path.exists(output):
                 return output
 
-            await self.save_thumb(temp, song.thumbnail)
+             
+            custom_image_url = "https://files.catbox.moe/agqvg6.jpg"
 
-            await asyncio.to_thread(self._draw_image, temp, output, song, size)
+            
+            temp_filename = f"cache/temp_{song.id}.jpg"
 
+            
+            await self.save_thumb(temp_filename, custom_image_url)
+
+            
+            await asyncio.to_thread(self._draw_image, temp_filename, output, size)
+
+            
             try:
-                os.remove(temp)
+                os.remove(temp_filename)
             except Exception:
                 pass
             return output
         except Exception:
+            
             return config.DEFAULT_THUMB
