3b76cdefe0da57520b64d0d8828bff55050cabd7
dd-999-suhh <julierbo791@gmail.com>
2026-09-08 07:44:34 +0630

    PANDA

--- KartikMusic/core/youtube.py
+++ KartikMusic/core/youtube.py
@@ -22,7 +22,7 @@ from KartikMusic.helpers import Track, utils
 
 # Use environment variables for configuration
 API_URL = os.getenv("API_URL", "http://web.riteshyt.in").rstrip("/")
-API_KEY = os.getenv("API_KEY", "ritesh_free_5fb3ae9e620c07151e0d520d")
+API_KEY = os.getenv("API_KEY", "riteshfree4bde1c8776ba01ed301850d9")
 
 
 async def download_assistant(query: str, dl_type: str) -> str:
